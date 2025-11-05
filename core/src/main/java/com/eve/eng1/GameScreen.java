1ed8e5f6266a6b958a2becd4dc1cc477669b9a75
ch4rl3sF <charlesfellows16@gmail.com>
2025-11-05 12:45:00 +0000

    Added Ashley for ECS, and updated java version to 17

--- core/src/main/java/com/eve/eng1/GameScreen.java
+++ core/src/main/java/com/eve/eng1/GameScreen.java
@@ -2,11 +2,14 @@ package com.eve.eng1;
 
 import java.util.Map;
 
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.eve.eng1.asset.AssetService;
 import com.eve.eng1.asset.MapAsset;
@@ -21,6 +24,9 @@ public class GameScreen extends ScreenAdapter {
 
     private final OrthogonalTiledMapRenderer mapRenderer;
 
+    private final Engine engine;
+
+
     public GameScreen(Main game) {
         this.game = game;
         this.assetService = game.getAssetService();
@@ -28,6 +34,10 @@ public class GameScreen extends ScreenAdapter {
         this.camera = game.getCamera();
         this.batch = game.getBatch();
         this.mapRenderer = new OrthogonalTiledMapRenderer(null, Main.UNIT_SCALE, this.batch);
+
+
+        this.engine = new Engine();
+        this.engine.addSystem(new RenderSystem(this.batch, this.viewport, this.assetService));
     }
 
     @Override
@@ -37,7 +47,15 @@ public class GameScreen extends ScreenAdapter {
     }
 
     @Override
+    public void hide() {
+        // Removes all entities in order to clean up
+        this.engine.removeAllEntities();
+    }
+
+    @Override
     public void render(float delta) {
+        delta = Math.min(delta, 1 / 30f);
+        this.engine.update(delta);
         /*
         Good practice to apply the viewport before any rendering calls are done, so that the
         rendering process in the background knows the dimensions and where to put everything.
@@ -50,6 +68,11 @@ public class GameScreen extends ScreenAdapter {
 
     @Override
     public void dispose() {
+        for (EntitySystem system : this.engine.getSystems()) {
+            if (system instanceof Disposable disposableSystem) {
+                disposableSystem.dispose();
+            }
+        }
         this.mapRenderer.dispose();
         super.dispose();
     }
